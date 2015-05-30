bf58b02f256b7ba150690b024cd35ccacbfa9d0e
Vegard Stene <vstene@conta.no>
2015-05-30 23:36:10 +0700

    Add solution to lesson 3 - passing cars

---
+++ lesson3/PassingCars.js
@@ -0,0 +1,24 @@
+/**
+ * Javascript solution to codility lesson 3 - Prefix Sums -> PassingCars.
+ * Score: 100/100
+ * https://codility.com/demo/take-sample-test/passing_cars/
+ */
+function solution(A) {
+    var passed = 0, pairs = 0;
+
+    for (var i = 0; i < A.length; i++) {
+        if (A[i] === 0) {
+            passed += 1;
+        } else {
+            pairs += passed;
+        }
+
+        if (pairs > 1000000000) {
+            pairs = -1;
+
+            break;
+        }
+    }
+
+    return pairs;
+}
